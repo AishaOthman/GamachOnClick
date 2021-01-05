76ccef013747161d4e80ed8f61bec3bcfbfed869
AishaOthman <75293286+AishaOthman@users.noreply.github.com>
2021-01-05 21:49:41 +0200

    Add files via upload

---
+++ Gamach.js
@@ -0,0 +1,34 @@
+let Gamach={
+			firstName,
+			lastName,
+			email,
+			password,
+			Area,
+			telephonNumber,
+			item,
+			client,
+			addItem(){},
+			removeItem(){},
+			addClient(){},
+			removeClient(){}
+};
+
+let Item={
+		id,
+		name,
+		catogory,
+		images,
+		statuse,
+		details
+};
+
+let client={
+			firstName,
+			lastName,
+			reservedItemId,
+			fromDate,
+			toDate,
+			telephonNumber,
+			email,
+			payment
+};
