0cce5bf16f4f27e339153c2ae90479532b61b875
AishaOthman <75293286+AishaOthman@users.noreply.github.com>
2021-01-18 09:57:22 +0200

    Add files via upload

---
+++ userController.js
@@ -0,0 +1,12 @@
+exports.logIn = function(){
+
+}
+exports.logOut = function(){
+    
+}
+exports.register = function(req,res){
+    res.send('Thanks for trying to register')
+}
+exports.home = function(req,res){
+    res.render('home-guest')
+}
